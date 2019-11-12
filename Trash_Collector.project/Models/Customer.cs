2e98e17ba47c8b5c5461d76ec70e4277584088fd
MrMencia <Avimencia@gmail.com>
2019-11-11 22:47:55 -0600

    added customer model

---
+++ Trash_Collector.project/Models/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Trash_Collector.project.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Pickup Day")]
+        public string PickupDay { get; set; }
+
+        [Display(Name = "Add Additional Pick Day")]
+        public DateTime AdditionalPickupDate { get; set; }
+
+        [Display(Name = "Suspend Service: Start Date")]
+        public DateTime SuspendStartDate { get; set; }
+
+        [Display(Name = "Suspend Service: End Date")]
+        public DateTime SuspendEndDate { get; set; }
+        
+        [Display(Name = "Account Balance")]
+        public double AccountBalance { get; set; }
+
+    }
+}
